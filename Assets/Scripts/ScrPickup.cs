ed8d1530dcb5abb5e49ce9fa680aedfe20f92a3b
Helena Martí <helenamarti99@gmail.com>
2020-11-30 10:44:08 +0100

    02 Player_Pickup
    Importació d'assets amb funció de placeholder fins que estiguin creats els finals.
    Creació Player i el seu moviment.
    Creació Pickup, col·lisions i destrucció.

---
+++ Assets/Scripts/ScrPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Script utilitzat per al control dels Pickups
+/// AUTOR:  Helena Martí Barragán
+/// DATA:   30/11/2020
+/// VERSIÓ: 1.0
+/// CONTROL DE VERSIONS
+///         1.0: primera versió. Definició del gir de l'sprite.
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrPickup : MonoBehaviour
+{
+    [SerializeField] float velocitatGir = 1;
+
+    public int valor = 1;   // valor de puntuació que valdrà cada pickup.
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, velocitatGir * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Script utilitzat per al control del Player
+/// AUTOR:  Helena Martí Barragán
+/// DATA:   30/11/2020
+/// VERSIÓ: 1.0
+/// CONTROL DE VERSIONS
+///         1.0: primera versió. Moviment del player en els eixos xy, col·lisió i destrucció pickups.
+/// ----------------------------------------------------------------------------------
+/// </summary>
+    
+public class ScrPlayer : MonoBehaviour
+{
+    float x, y; // variables per a guardar la lectura dels cursors
+
+    [SerializeField] float forsa = 10;
+
+    Rigidbody2D rb;
+    ScrPickup scrP;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // llegim el moviment als dos eixos
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x * forsa, y * forsa));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pickup"))
+        {
+            scrP = collision.GetComponent<ScrPickup>();
+            Destroy(collision.gameObject);
+        } 
+    }
+}
