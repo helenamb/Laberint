f3d95a2b4e9986dadeed73bb028b970de624458c
Helena <71768197+helenamb@users.noreply.github.com>
2020-12-31 12:32:21 +0100

    03 Assets, UI
    Canvi dels assets placeholder per els definitius
    Definició de les col·lisions de l'escenari, el player i el pickup
    Script per a que la càmera no canvii de relació d'aspecte independentment de la resolució
    Creació de la UI, funcionament del crono

---
+++ Assets/Scripts/ScrControlGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Script utilitzat per a gestionar les accions del joc
+/// AUTOR:  Helena Martí
+/// DATA:   31/12/2020
+/// VERSIÓ: 1.0
+/// CONTROL DE VERSIONS
+///         1.0: primera versió.
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrControlGame : MonoBehaviour
+{
+    public static int punts = 0;
+    public static int vida = 0;
+    //public static int pickups = 0;            // no se si ho necessitarem, no comptabilitzo els pickups que falten
+
+    private void Start()
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/ScrUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   // llibreria per a utilitzar els elements de tipus UI
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Script utilitzat per a gestionar les accions relacionades amb la UI
+/// AUTOR:  Helena Martí
+/// DATA:   31/12/2020
+/// VERSIÓ: 1.0
+/// CONTROL DE VERSIONS
+///         1.0: primera versió. Mostra puntuació, vida i temps
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrUI : MonoBehaviour
+{
+    [SerializeField]
+    Text puntuacio, vida, temps;
+
+    float crono = 0;
+
+    void Update()
+    {
+        crono += Time.deltaTime;
+        puntuacio.text = "" + ScrControlGame.punts;
+        vida.text = "" + ScrControlGame.vida;
+        temps.text = "" + crono.ToString("0.0");
+    }
+}
